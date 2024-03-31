896feae277700dfb4993205f5d22b01590ec82d2
KimBenE <kimbenelisha47@gmail.com>
2024-03-31 21:29:29 +0300

    wip

--- www/app.js
+++ www/app.js
@@ -4,43 +4,47 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const path = require('path');
+
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
+
 const db = require('./app/db');
 require('./dbInit');
 
-// Serve static files from the 'uploads/' folder
-app.use(express.static(path.join(__dirname, 'uploads')));
+// Allow all clients to connect to our server
+// app.use(cors());
 
-// Allow requests from localhost
-const allowedOrigins = ['http://localhost', 'http://adopeti.xyz'];
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+// Allow requests from specific origin
+const allowedOrigin = ['http://adopeti.xyz:3000', 'http://localhost'];
+const corsOptions = {
+  origin: allowedOrigin
+};
+
+// Use cors middleware with options
+app.use(cors(corsOptions));
 
 // Use body-parser middleware to parse JSON requests
-app.use(bodyParser.urlencoded({
-    extended: false
- }));
- 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Import route modules
 const usersRoutes = require('./app/routes/users.js');
 const animalsRoutes = require('./app/routes/animals.js');
 const associationsRoutes = require('./app/routes/associations.js');
+const requestsRoutes = require('./app/routes/requests.js');
 
 // Mount routes 
 app.use('/users', usersRoutes);
 app.use('/animals', animalsRoutes);
 app.use('/associations', associationsRoutes);
+app.use('/requests', requestsRoutes);
 
-const host = '0.0.0.0';
+// Serve static files
+app.use(express.static(path.join(__dirname, '.')));
+app.use(express.static(path.join(__dirname, 'assets/html')));
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Start the server
 app.listen(port, host, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
